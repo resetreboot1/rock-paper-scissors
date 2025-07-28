0eb3a03d763f435e29f08cf629571f5d964899c8
resetreboot1 <206232704+resetreboot1@users.noreply.github.com>
2025-07-28 21:26:19 +0600

    Make the game playable in console

---
+++ javascript.js
@@ -0,0 +1,49 @@
+function getComputerChoice() {
+   if (compChoice <= 0.33) {
+    return "rock"
+   } else if (compChoice <= 0.66) {
+    return "paper"
+   } else {
+    return "scissors"
+   }
+   
+}
+
+function pointDistributor() {
+       if ((properChoice === "rock" && getComputerChoice() === "scissors")
+       || (properChoice === "scissors" && getComputerChoice() === "paper")
+       || (properChoice === "paper" && getComputerChoice() === "rock" )) {
+        ++playerPoints
+      }else if ((properChoice === "rock" && getComputerChoice() === "paper")
+          || (properChoice === "paper" && getComputerChoice() === "scissors")
+          || (properChoice === "scissors" && getComputerChoice() === "rock")) {
+            ++ComputerPoints
+         }else if (properChoice === getComputerChoice()) {
+            }
+            else {
+                return console.log("error. Try again.")
+            }
+        }
+
+let playerPoints = 0;
+let ComputerPoints = 0;
+let properChoice;
+
+function playGame() {
+    
+    choice = prompt("Choose: Rock, Paper or Scissors");
+     properChoice = choice.toLowerCase();
+     compChoice = Math.random();
+     getComputerChoice()
+     pointDistributor()
+         if (playerPoints === 3) {
+         return console.log("congrats Player. You win.")
+          }else if (ComputerPoints === 3) {
+          return  console.log("Maybe next time player. Computer wins.")
+          }else {
+           return console.log("Player Points: " + playerPoints + ", " + "Computer Points: " + ComputerPoints);
+          }
+        }
+
+
+
