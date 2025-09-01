57e36754f6d17ff38793a7191c4205ad02e38e88
resetreboot1 <206232704+resetreboot1@users.noreply.github.com>
2025-09-01 21:42:30 +0600

    Add rest of the UI to see the score and the current result
    also made the game stop at 5 points instead of 3 points.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 let playerPoints = 0;
 let ComputerPoints = 0;
 let playerChoice;
+let resultsContainer;
+let pointsContainer;
 
 function getComputerChoice() {
    if (compChoice <= 0.33) {
@@ -18,46 +20,63 @@ function distritbutePoints(properChoice) {
        || (properChoice === "scissors" && getComputerChoice() === "paper")
        || (properChoice === "paper" && getComputerChoice() === "rock" )) {
        ++playerPoints
+       return resultsContainer.textContent = `The computer has chosen ${getComputerChoice()}.`;
+       
   }
   else if ((properChoice === "rock" && getComputerChoice() === "paper")
           || (properChoice === "paper" && getComputerChoice() === "scissors")
           || (properChoice === "scissors" && getComputerChoice() === "rock")) {
           ++ComputerPoints
+       return resultsContainer.textContent = `The computer has chosen ${getComputerChoice()}.`;
   }
   else if (properChoice === getComputerChoice()) {
+       return resultsContainer.textContent = "Both of you have chosen the same. No points awarded."
   }
-  else {
-        return console.log("error. Try again.")
-  }
+  
 }
 
 function playGame(properChoice) {
      compChoice = Math.random();
      getComputerChoice()
      distritbutePoints(properChoice)
-         if (playerPoints === 3) {
-         return console.log("congrats Player. You win.")
-          }else if (ComputerPoints === 3) {
-          return  console.log("Maybe next time player. Computer wins.")
-          }else {
-           return console.log("Player Points: " + playerPoints + ", " + "Computer Points: " + ComputerPoints);
-          }
+        if (playerPoints === 5) {
+         resultsContainer.textContent = "congrats Player. You win.";
+          container.removeEventListener("click", returnDisplay);
         }
+        else if (ComputerPoints === 5) {
+         resultsContainer.textContent = "Maybe next time player. Computer wins.";
+         container.removeEventListener("click", returnDisplay);   
+        }
+}
+
+function returnPoints() {
+ return "Player Points: " + playerPoints + ", " + "Computer Points: " + ComputerPoints;
+}
 
 
 
-let rock = document.querySelector(".rock")
-let paper = document.querySelector(".paper")
-let scissors = document.querySelector(".scissors")
-let container = document.querySelector(".button-container")
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+let container = document.querySelector(".button-container");
+
+pointsContainer = document.createElement("div");
+pointsContainer.textContent = returnPoints();
+document.body.insertBefore(pointsContainer, container)
+
+resultsContainer = document.createElement("div");
+resultsContainer.textContent = "Click on any choice to play the game.";
+document.body.appendChild(resultsContainer) 
+
 
 let choice;
-container.addEventListener("click", (e) => {
+container.addEventListener("click", returnDisplay = (e) => {
   choice = (e.target.textContent)
   if (choice === "rock"   ||
       choice === "paper"  ||
       choice === "scissors") {
       playGame(choice)
+      pointsContainer.textContent = returnPoints();
     }
 })
 
